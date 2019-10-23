d5c341f8e923df149e49cd6a416194b7996d234e
Turan Ketene <turank97@gmail.com>
2019-10-23 11:57:23 -0700

    created home page and friends.js

---
+++ app/data/friends.js
@@ -0,0 +1,68 @@
+var friends = [
+    {
+        name: "Arda Turan",
+        photo: "https://ichef.bbci.co.uk/news/660/cpsprodpb/4BED/production/_103873491_gettyimages-624383778.jpg",
+        scores: [
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1"
+        ]        
+    },
+    {
+        name: "Lebron James",
+        photo: "https://image.cnbcfm.com/api/v1/image/105642793-1545408067309gettyimages-1074484904.jpeg?v=1545408690&w=1400&h=950",
+        scores: [
+            "3",
+            "2",
+            "5",
+            "4",
+            "4",
+            "5",
+            "2",
+            "3",
+            "3",
+            "2"
+        ]       
+    },
+    {
+        name: "Bill Gates",
+        photo: "https://3er1viui9wo30pkxh1v2nh4w-wpengine.netdna-ssl.com/wp-content/uploads/prod/2014/08/BillGatesHeadshot.jpg",
+        scores: [
+            "5",
+            "3",
+            "5",
+            "3",
+            "2",
+            "2",
+            "5",
+            "5",
+            "2",
+            "1"
+        ]       
+    },
+    {
+        name: "Ryan Reynolds",
+        photo: "https://upload.wikimedia.org/wikipedia/commons/1/14/Deadpool_2_Japan_Premiere_Red_Carpet_Ryan_Reynolds_%28cropped%29.jpg",
+        scores: [
+            "3",
+            "4",
+            "4",
+            "4",
+            "4",
+            "1",
+            "5",
+            "5",
+            "5",
+            "5"
+        ]       
+    }
+];
+// Exporting the friends array so that it can be used by apiRoutes.js to display to user
+module.exports = friends;
